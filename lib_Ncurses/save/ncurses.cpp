55784ef1dcbead198bad5628084287df611cf7de
lao_e <lao_e@epitech.eu>
2015-04-05 18:23:00 +0200

    Ncurses save

---
+++ lib_Ncurses/save/ncurses.cpp
@@ -0,0 +1,208 @@
+//
+// ncurses.cpp for  in /home/lao_e/rendu/snake_de_ouf/lib_Ncurses
+// 
+// Made by Aurélie LAO
+// Login   <lao_e@epitech.net>
+// 
+// Started on  Fri Apr  3 15:14:38 2015 Aurélie LAO
+// Last update Sun Apr  5 17:56:49 2015 Aurélie LAO
+//
+
+#include <ncurses/curses.h>
+#include <unistd.h>
+#include "../header/the_snake.hh"
+#include "../header/my_exception.hh"
+#include <string>
+
+bool curses_started = false;
+
+bool		check_coord(int x, int y, Snake *s)
+{
+  std::list<int>::iterator it = s->_table.begin();
+
+  while (it != s->_table.end())
+    {
+      if ((x != s->_x_max) && (x == *it))
+	{
+	  ++it;
+	  if ((y != s->_y_max) && (y == *it))
+	    return (true);
+	}
+      else
+	++it;
+      ++it;
+    }
+  return (false);
+  
+}
+
+void		print_snake(int y, Snake *s)
+{
+  int		x = 1;
+  std::string		str;
+
+  str = "|";
+  while (x < s->_x_max - 1)
+    {
+      if (y == s->_y_eat && x == s->_x_eat)
+	str = str + "*";
+      else if (check_coord(x, y, s) == true)
+	str = str + "o";
+      else
+	str = str + " ";
+      ++x;
+    }
+  str = str + "|";
+  printw("%s\n", str.c_str());
+}
+
+void		print_map(Snake *s)
+{
+  int		x = s->_x_max;
+  int		y = s->_y_max - 1;
+  std::string	first_str;
+
+  while (x > 0)
+    {
+      first_str = first_str + "-";
+      --x;
+    }
+  x = s->_x_max - 1;
+  printw("%s\n", first_str.c_str());
+  while (y > 0)
+    {
+      print_snake(y, s);
+      --y;
+    }
+  printw("%s\n", first_str.c_str());
+}
+
+void	endCurses()
+{
+  if (curses_started && !isendwin())
+    if (endwin() != OK)
+      throw My_exception(1, "Close window failed");
+}
+
+void	startCurses()
+{
+  if (curses_started)
+    {
+      if (refresh() != OK)
+	throw My_exception(1, "Refresh window failed");
+    }
+  else
+    {
+      curses_started = true;
+      if (initscr() == NULL || cbreak() != 0 /*recup 1 seule touche*/|| noecho() != 0/*n'affiche pas la toucher pressée*/)
+       	throw My_exception(1, "Initialisation Ncurses failed");
+      curs_set(0);
+    }
+}
+
+void	print_end()
+{
+  printw("\n\n\n                %s\n", "END");
+  printw("\n%s\n", "Nibbler by Marie TROTIER & Aurelie LAO");
+  printw("\n\n\n       %s\n", "Press Enter to exit");
+}
+
+void	print_stat(Snake *s)
+{
+  printw("eat = (%d, %d)\n%s\n", s->_x_eat, s->_y_eat, "List =");
+  for (std::list<int>::iterator it = s->_table.begin(); it != s->_table.end(); it++)
+    printw("%d\n", *it);
+}
+
+void	my_print(Snake *s)
+{
+  int	key;
+
+  print_map(s);
+  while ((key = getch()) != 10)
+    {
+      clear();
+      if (s->check_is_alive() != false && s->_is_alive != false)
+      	{
+	  switch (key)
+	    {
+	    case 65: //UP
+	      if (s->_dir != Down)
+		{
+		  s->go_up();
+		  if (s->check_is_alive() == false)
+		    print_end();
+		  else
+		    {
+		      s->check_can_eat();
+		      print_map(s);
+		      print_stat(s);
+		    }
+		}
+	      else
+		print_map(s);
+	      break;
+	    case 66: //DOWN
+	      if (s->_dir != Up)
+		{
+		  s->go_down();
+		  if (s->check_is_alive() == false)
+		    print_end();
+		  else
+		    {
+		      s->check_can_eat();
+		      print_map(s);
+		      print_stat(s);
+		    }
+		}
+	      else
+		print_map(s);
+	      break;
+	    case 67: //RIGHT
+	      if (s->_dir != Left)
+		{
+		  s->turn_right();
+		  if (s->check_is_alive() == false)
+		    print_end();
+		  else
+		    {
+		      s->check_can_eat();
+		      print_map(s);
+		      print_stat(s);
+		    }
+		}
+	      else
+		print_map(s);
+	      break;
+	    case 68: //LEFT
+	      if (s->_dir != Right)
+		{
+		  s->turn_left();
+		  if (s->check_is_alive() == false)
+		    print_end();
+		  else
+		    {
+		      s->check_can_eat();
+		      print_map(s);
+		      print_stat(s);
+		    }
+		}
+	      else
+		print_map(s);
+	      break;
+	    default:
+	      break;
+	    }
+	}
+      else
+	print_end();
+    }
+}
+void		go_Ncurses(Snake *s)
+{
+  check_status(s);
+  s->check_can_eat();
+  startCurses();
+  my_print(s);
+  endCurses();
+}
---
+++ lib_Ncurses/save/the_snake.cpp
@@ -0,0 +1,228 @@
+//
+// the_window.cpp for  in /home/lao_e/rendu/snake_de_ouf/lib_Ncurses
+// 
+// Made by Aurélie LAO
+// Login   <lao_e@epitech.net>
+// 
+// Started on  Wed Apr  1 15:59:55 2015 Aurélie LAO
+// Last update Sun Apr  5 18:17:54 2015 Aurélie LAO
+//
+
+#include "../header/the_snake.hh"
+#include <stdlib.h>
+
+Snake::Snake(int x, int y)
+{
+  this->_table.push_back(x / 2);
+  this->_table.push_back(y / 2);
+  this->_table.push_back(x / 2);
+  this->_table.push_back((y / 2) - 1);
+  this->_table.push_back(x / 2);
+  this->_table.push_back((y / 2) - 2);
+
+  this->_x_max = x;
+  this->_y_max = y;
+  while ((this->_x_eat = rand() % x) == 0 || this->_x_eat == rand() % (x / 2))
+    rand();
+  while ((this->_y_eat = rand() % y) == 0 ||
+	 this->_y_eat ==(y / 2) ||
+	 this->_y_eat == (y / 2 - 1) ||
+	 this->_y_eat == (y / 2 - 2))
+    rand();
+  this->_is_alive = true;
+  this->_have_meal = false;
+  this->_dir = Up;
+}
+
+Snake::~Snake()
+{
+}
+
+void	Snake::turn_left()
+{
+  int	x;
+  int	y;
+
+  this->_dir = Left;
+  if (this->_have_meal == 0)
+    {
+      this->_table.pop_back();
+      this->_table.pop_back();
+    }
+  else
+    this->_have_meal = this->_have_meal - 1;
+  x = this->_table.front();
+  this->_table.pop_front();
+  y =  this->_table.front();
+  this->_table.pop_front();
+  this->_table.push_front(y);
+  this->_table.push_front(x);
+  this->_table.push_front(y);
+  if (x - 1 <= 0)
+    this->_is_alive = false;
+  this->_table.push_front(x - 1);
+}
+
+void	Snake::turn_right()
+{
+  int	x;
+  int	y;
+
+  this->_dir = Right;
+  if (this->_have_meal == 0)
+    {
+      this->_table.pop_back();
+      this->_table.pop_back();
+    }
+  else
+    this->_have_meal = this->_have_meal - 1;
+  x = this->_table.front();
+  this->_table.pop_front();
+  y =  this->_table.front();
+  this->_table.pop_front();
+  this->_table.push_front(y);
+  this->_table.push_front(x);
+  this->_table.push_front(y);
+  if (x + 1 >= this->_x_max)
+    this->_is_alive = false;
+  this->_table.push_front(x + 1);
+}
+
+void	Snake::go_up()
+{
+  int	x;
+  int	y;
+
+  this->_dir = Up;
+  if (this->_have_meal == 0)
+    {
+      this->_table.pop_back();
+      this->_table.pop_back();
+    }
+  else
+    this->_have_meal = this->_have_meal - 1;
+  x =  this->_table.front();
+  this->_table.pop_front();
+  y = this->_table.front();
+  this->_table.pop_front();
+  this->_table.push_front(y);
+  this->_table.push_front(x);
+  if (y + 1 >= this->_y_max)
+    this->_is_alive = false;
+  this->_table.push_front(y + 1);
+  this->_table.push_front(x);
+}
+
+void	Snake::go_down()
+{
+  int	x;
+  int	y;
+
+  this->_dir = Down;
+  if (this->_have_meal == 0)
+    {
+      this->_table.pop_back();
+      this->_table.pop_back();
+    }
+  else
+    this->_have_meal = this->_have_meal - 1;
+  x = this->_table.front();
+  this->_table.pop_front();
+  y =  this->_table.front();
+  this->_table.pop_front();
+  this->_table.push_front(y);
+  this->_table.push_front(x);
+  if (y - 1 <= 0)
+    this->_is_alive = false;
+  this->_table.push_front(y - 1);
+  this->_table.push_front(x);
+}
+
+void	Snake::go_continu()
+{
+  switch (this->_dir)
+    {
+    case Up:
+      go_up();
+      break;
+    case Down:
+      go_down();
+      break;
+    case Right:
+      turn_right();
+      break;
+    case Left:
+      turn_left();
+      break;
+    default:
+      break;
+    }
+}
+
+bool	Snake::check_is_alive()
+{
+  int	first_x;
+  int	first_y;
+
+  first_x = this->_table.front();
+  this->_table.pop_front();
+  first_y = this->_table.front();
+  this->_table.pop_front();
+  if (first_x == 0 || first_x == this->_x_max ||
+      first_y == 0 || first_y == this->_y_max)
+    {
+      this->_is_alive = false;
+      this->_table.push_front(first_y);
+      this->_table.push_front(first_x);
+      return (false);
+    }
+  for (std::list<int>::iterator it = this->_table.begin(); it != this->_table.end(); it++)
+    {
+      if (first_x == *it)
+	{
+	  ++it;
+	  if (first_y == *it)
+	    {
+	      this->_table.push_front(first_y);
+	      this->_table.push_front(first_x);
+	      this->_is_alive = false;
+	      return (false);
+	    }
+	  --it;
+	}
+      ++it;
+    }
+  this->_table.push_front(first_y);
+  this->_table.push_front(first_x);
+  return (true);
+}
+
+bool	Snake::check_can_eat()
+{
+  std::list<int>::iterator it = this->_table.begin();
+  int	count = 0;
+
+  while (it != this->_table.end())
+    {
+      if (*it == this->_x_eat)
+	++count;
+      ++it;
+      if (count != 0 && *it == this->_y_eat)
+	{
+	  this->_y_eat = this->_table.back();
+	  this->_table.pop_back();
+	  this->_x_eat = this->_table.back();
+	  this->_table.push_back(this->_x_eat);
+	  this->_have_meal = 2;
+	  return (true);
+	}
+      else if (it == this->_table.end())
+	{
+	  this->_have_meal = 2;
+	  return (false);
+	}
+      ++it;
+    }
+  this->_have_meal = 0;
+  return (false);
+}
